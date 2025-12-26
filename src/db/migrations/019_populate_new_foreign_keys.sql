1e9bd323955d010668d5a6f439df0c2ff916936a
yergozhin <erzhanghg@gmail.com>
2025-12-26 16:26:47 +0100

    fixed

--- src/db/migrations/019_populate_new_foreign_keys.sql
+++ src/db/migrations/019_populate_new_foreign_keys.sql
@@ -1,36 +1,84 @@
-update fights f
-set fighter_a_profile_id = fp.id
-from fighter_profiles fp
-where f.fighter_a_id = fp.user_id
-  and f.fighter_a_profile_id is null;
+do $$
+begin
+  if exists (
+    select 1 from information_schema.columns 
+    where table_name = 'fights' and column_name = 'fighter_a_id'
+  ) then
+    update fights f
+    set fighter_a_profile_id = fp.id
+    from fighter_profiles fp
+    where f.fighter_a_id = fp.user_id
+      and f.fighter_a_profile_id is null;
+  end if;
+end $$;
 
-update fights f
-set fighter_b_profile_id = fp.id
-from fighter_profiles fp
-where f.fighter_b_id = fp.user_id
-  and f.fighter_b_profile_id is null;
+do $$
+begin
+  if exists (
+    select 1 from information_schema.columns 
+    where table_name = 'fights' and column_name = 'fighter_b_id'
+  ) then
+    update fights f
+    set fighter_b_profile_id = fp.id
+    from fighter_profiles fp
+    where f.fighter_b_id = fp.user_id
+      and f.fighter_b_profile_id is null;
+  end if;
+end $$;
 
-update events e
-set plo_profile_id = pp.id
-from plo_profiles pp
-where e.plo_id = pp.user_id
-  and e.plo_profile_id is null;
+do $$
+begin
+  if exists (
+    select 1 from information_schema.columns 
+    where table_name = 'events' and column_name = 'plo_id'
+  ) then
+    update events e
+    set plo_profile_id = pp.id
+    from plo_profiles pp
+    where e.plo_id = pp.user_id
+      and e.plo_profile_id is null;
+  end if;
+end $$;
 
-update offers o
-set fighter_profile_id = fp.id
-from fighter_profiles fp
-where o.fighter_id = fp.user_id
-  and o.fighter_profile_id is null;
+do $$
+begin
+  if exists (
+    select 1 from information_schema.columns 
+    where table_name = 'offers' and column_name = 'fighter_id'
+  ) then
+    update offers o
+    set fighter_profile_id = fp.id
+    from fighter_profiles fp
+    where o.fighter_id = fp.user_id
+      and o.fighter_profile_id is null;
+  end if;
+end $$;
 
-update offers o
-set plo_profile_id = pp.id
-from plo_profiles pp
-where o.plo_id = pp.user_id
-  and o.plo_profile_id is null;
+do $$
+begin
+  if exists (
+    select 1 from information_schema.columns 
+    where table_name = 'offers' and column_name = 'plo_id'
+  ) then
+    update offers o
+    set plo_profile_id = pp.id
+    from plo_profiles pp
+    where o.plo_id = pp.user_id
+      and o.plo_profile_id is null;
+  end if;
+end $$;
 
-update fighter_verifications fv
-set fighter_profile_id = fp.id
-from fighter_profiles fp
-where fv.fighter_id = fp.user_id
-  and fv.fighter_profile_id is null;
+do $$
+begin
+  if exists (
+    select 1 from information_schema.columns 
+    where table_name = 'fighter_verifications' and column_name = 'fighter_id'
+  ) then
+    update fighter_verifications fv
+    set fighter_profile_id = fp.id
+    from fighter_profiles fp
+    where fv.fighter_id = fp.user_id
+      and fv.fighter_profile_id is null;
+  end if;
+end $$;
 
